06ab5b5ac4202dd5ef8118b3b5ea3bc00971c32e
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-02-02 22:57:05 -0500

    add mongoose to index.js and connect to database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://Test:418763@cluster0.dyqhe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 app.listen(3001, () => {
   console.log("SEVER IS RUNNING")
