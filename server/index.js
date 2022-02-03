88b44d8c704f17293839a5fad9956517d0637449
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-02-02 23:16:54 -0500

    make get request and test on thunder client

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
-const express = require("express")
-const app = express()
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const UserModel = require('./models/users');
+mongoose.connect(
+  'mongodb+srv://Test:418763@cluster0.dyqhe.mongodb.net/sample_airbnb?retryWrites=true&w=majority'
+);
 
-mongoose.connect("mongodb+srv://Test:418763@cluster0.dyqhe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+// would actually go in routes.js
+app.get('/getUsers', (req, res) => {
+  UserModel.find({}, (error, result) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json(result);
+    }
+  });
+});
+// app.post()
 
 app.listen(3001, () => {
-  console.log("SEVER IS RUNNING")
+  console.log('SEVER IS RUNNING');
 });
