37dbddca61385e3b48f047fe01fc76cf4c8b2af7
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-02-02 23:46:10 -0500

    make post request connect to db, add input fields

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import Axios from 'axios';
+import axios from 'axios';
 
 function App() {
   const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState('');
 
   useEffect(() => {
-    Axios.get('http://localhost:3001/getUsers').then((response) => {
+    axios.get('http://localhost:3001/getUsers').then((response) => {
       setListOfUsers(response.data);
     });
   }, []);
+
+  const createUser = () => {
+    axios
+      .post('http://localhost:3001/createUser', {
+        name,
+        age,
+        username,
+      })
+      .then((response) => {
+        alert('USER CREATED');
+      });
+  };
   return (
     <div className="App">
       <div className="usersDisplay">
@@ -22,6 +37,24 @@ function App() {
             </div>
           );
         })}
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Age"
+          onChange={(event) => setAge(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <button onClick={createUser}>Create User</button>
       </div>
     </div>
   );
